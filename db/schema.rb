3b8f59902de2bc3acbc8ee5249745ddb9a11aae4
macos <macos@macosnoMacBook-Pro.local>
2023-07-14 01:57:52 +0900

    キリがいいのでpushします。投稿機能の追加。ユーザー機能の追加作業中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_140353) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_154223) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
